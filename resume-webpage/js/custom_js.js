2c133df5907348854995638bc3c94fedb4f04b2d
Blake Conrad <bmconrad@iupui.edu>
2017-01-26 12:02:50 -0500

    most recent changes

---
+++ resume-webpage/js/custom_js.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+
+});
+
+
+var edu_btn = true;
+var hobbies_btn = true;
+var prog_lang_btn = true;
+
+function appendText(btn) {
+    
+    // If Button is a "+"
+    if(btn.id == "education-btn" && (edu_btn == true)){
+        edu_btn=false;
+        $("#education-btn").css({backgroundColor: "#800000"});
+        $('#education-btn').text(function(i, oldText) {
+            return oldText === '+' ? '-' : oldText;
+        });
+        
+        $('#appendHere').append("<ul><li><h4>Academic</h4><ul><li>B.S. in Computer Science from <a href='http://cs.iupui.edu' target='_blank'>Purdue University</a>.</li><li>Minor in Mathematics.</li></li></ul></ul><ul><li><h4>Professional</h4><ul><li>Developed, refined, and innovated new solutions to large scale software projects in an internship with the <a href='http://www.trac.army.mil/' target='_blank'>Department of Defense</a>.</li></li></ul>");
+        
+        
+        /*
+<ul id="education_appended">
+<li>B.S. in Computer Science from Purdue University.</li>
+<li>Minor in Mathematics.</li>
+<li>Developed, refined, and innovated new solutions to large scale software projects 
+in an internship with the Department of Defense.</li>
+</ul>
+        */
+        
+    // If Button is a "-"
+    }else if (btn.id == "education-btn" && !(edu_btn==true)){
+        edu_btn=true;
+        $('#education-btn').text(function(i, oldText) {
+            return oldText === '-' ? '+' : oldText;
+        });
+        $("#education-btn").css({backgroundColor: "#483D8B"});
+        $('#appendHere').empty();
+        
+    }
+    
+    // If Button is a "+"
+    else if(btn.id == "hobbies-btn" && (hobbies_btn == true)){
+        hobbies_btn=false;
+        $("#hobbies-btn").css({backgroundColor: "#800000"});
+        $('#hobbies-btn').text(function(i, oldText) {
+            return oldText === '+' ? '-' : oldText;
+        });
+        
+        $('#appendHere2').append("<ul><li><h4>Computed Science Related</h4><ul><li>Competing in <a href='https://www.kaggle.com/bmconrad' target='_blank'>Kaggle Competitions</a>.</li><li>Contributing to <a href='http://stackoverflow.com/users/4304526/bmc' target='_blank'>Stack Exchange Communities</a>.</li></ul> </li><li><h4>Non-Computer Science Related</h4><ul><li>Learning new ways to utilize my skills to help those around me in a <a href='http://indymeals.com' target='_blank'> meaningful way.</a></li><li>Understanding how I can help those around me <a href='https://www.facebook.com/The-Creative-Change-Project-303593586466151/' target='_blank'>in need.</a></li></ul></li></ul>");
+        /* raw text appended
+          <ul>
+              <li><h4>Computed Science Related</h4>
+                <ul>
+                    <li>Competing in Kaggle Competitions.</li>
+                    <li>Contributing to Stack Exchange Communities.</li>
+                </ul>
+              </li>
+
+            <li><h4>Non-Computer Science Related</h4>
+                <ul>
+                    <li>
+                        Learning new ways to utilize my skills 
+                        to help those around me in a 
+                        <a href="http://indymeals.com">
+                        meaningful way.
+                        </a>
+                    </li>
+                    <li>
+                        Understanding how I can help those around me 
+                        <a href="https://www.facebook.com/The-Creative-Change-Project-303593586466151/">
+                        in need.</a>
+                    </li>
+                </ul>
+              </li>
+            </ul>
+        */
+        
+    // If Button is a "-"
+    }else if (btn.id == "hobbies-btn" && !(hobbies_btn==true)){
+        hobbies_btn=true;
+        $("#hobbies-btn").css({backgroundColor: "#483D8B"});
+        $('#hobbies-btn').text(function(i, oldText) {
+            return oldText === '-' ? '+' : oldText;
+        });
+        $('#appendHere2').empty();
+        
+    }
+    else if (btn.id == "prog-lang-btn" &&  (prog_lang_btn==true)){
+        prog_lang_btn=false;
+        $("#prog-lang-btn").css({backgroundColor: "#800000"});
+        $('#prog-lang-btn').text(function(i, oldText) {
+            return oldText === '+' ? '-' : oldText;
+        });
+        
+        $('#appendHere3').append("<ul><li><h4>Object Oriented Programming</h4><ul><li>C</li><li>C++</li><li>Python</li><li>Java</li></ul></li><li><h4>Procedural Scripting Programming</h4><ul><li>Batch (MS-DOS/Windows)</li><li>Bash (Unix/MAC)</li><li>Cshell (Linux)</li></ul></li><li><h4>Internet Programming</h4><ul><li>HTML5</li><li>CSS3</li><li>Javascript</li><li>Bootstrap</li><li>PHP</li><li>AJAX</li></ul><li><h4>Machine Learning &amp; Data Analysis Programming</h4><ul><li>Python (<a href='http://ipython.org/notebook.html' target='_blank'> IPython Notebook</a>, <a href='http://www.numpy.org' target='_blank'> NumPy</a>, <a href='http://pandas.pydata.org' target='_blank'> Pandas</a>, <a href='http://scipy.org/scipylib/index.html' target='_blank'>SciPy</a>, <a href='http://scikit-learn.org/stable/' target='_blank'>Scikit-learn </a>, and <a href='http://matplotlib.org' target='_blank'>Matplotlib</a> libraries) </li><li>R (<a href='https://github.com/topepo/caret' target='_blank'>caret</a>, <a href='https://github.com/yhat/ggplot' target='_blank'>ggplot2</a>, <a href='https://stat.ethz.ch/R-manual/R-patched/library/stats/html/glm.html' target='_blank'>Generalized Logistic Models</a>, <a href='https://cran.r-project.org/web/packages/randomForest/index.html' target='_blank'> Random Forest Models</a>, and <a href='https://cran.r-project.org/web/packages/AppliedPredictiveModeling/index.html' target='_blank'>other predictive modeling packages</a>)</li></ul></li><li><h4>Mobile Programming</h4><ul><li>Objective-C (iOS/Xcode)</li></ul></li><li><h4>Database Programming</h4><ul><li>SQL Server</li><li>MySQL</li></ul></li><li><h4>Functional Programming</h4><ul><li>Scheme</li></ul></li></ul></ul></ul>");
+    }
+    else if (btn.id == "prog-lang-btn" && !(prog_lang_btn==true)){
+        prog_lang_btn=true;
+        $("#prog-lang-btn").css({backgroundColor: "#483D8B"});
+        $('#prog-lang-btn').text(function(i, oldText) {
+            return oldText === '-' ? '+' : oldText;
+        });
+        $('#appendHere3').empty();
+    }
+};
+
+""
+    
+    
