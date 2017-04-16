9d1718637ec6a5609a98ac6ac13342853b6ddab3
Blake Conrad <bmconrad@iupui.edu>
2017-04-16 16:37:18 -0400

    working flask example w/o mysql yet

---
+++ Flask_Example/sql/create_bucketlist_schema.sql
@@ -0,0 +1,11 @@
+use Bucketlist;
+
+CREATE TABLE `BucketList`.`tbl_user` (
+  `user_id` BIGINT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NULL,
+  `user_username` VARCHAR(45) NULL,
+  `user_password` VARCHAR(45) NULL,
+  PRIMARY KEY (`user_id`));
+  
+select * from tbl_user;
+
